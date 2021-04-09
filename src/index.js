0a90b857b03e39043af916bd5890da3ba1d31eb5
Howakyl96 <howakyl@gmail.com>
2021-04-09 16:08:33 -0700

    intalled babel, gql server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('howdy')
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
+import { typeDefs, resolvers } from './schema';
+
+const app = express();
+
+const server = new ApolloServer ({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app }); // app is from an existing express app
+
+app.listen({ port: 4000 }, () => 
+  console.log(`server ready at http://localhost:4000${server.graphqlPath}`)
+)
